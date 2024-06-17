c9ba88529d5800e65289246aad08343c93356791
suhasledange <suhasledange10@gmail.com>
2024-06-17 21:57:52 +0530

    pad

--- components/ui/CardEffect.tsx
+++ components/ui/CardEffect.tsx
@@ -21,7 +21,7 @@ export function ThreeDCardDemo(
 ) {
   return (
     <CardContainer className="inter-var ">
-      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black-100 dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-10 border ">
+      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black-100 dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-4 md:p-8 border ">
         
         <CardItem translateZ="100" className="w-full">
           <Image
